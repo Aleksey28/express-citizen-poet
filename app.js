c421a78abc11160691536767705133b97201b3a6
Aleksey Popow <alekseypopow1995@yandex.ru>
2020-12-22 00:28:26 +0400

    первый комит

---
+++ app.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const auth = require('./middlewares/auth');
+
+const { register, login } = require('./controllers/auth');
+
+const { PORT = 3000, BASE_PATH } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/authdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/signup', register);
+app.post('/signin', login);
+
+app.use(auth);
+
+app.use('/posts', require('./routes/posts')); // добавился маршрут
+
+app.listen(PORT, () => {
+  console.log('Ссылка на сервер:');
+  console.log(BASE_PATH);
+});
---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const isEmail = require('validator/lib/isEmail');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 8,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправльные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправльные почта или пароль'));
+          }
+
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
